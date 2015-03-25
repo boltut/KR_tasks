a2d14b20aa4205127859006d31ff8dd4b71d07e6
Yuriy <yuriy.boltut@gmail.com>
2015-03-25 23:07:18 +0300

    Update fahr_in_func.c

--- fahr_in_func.c
+++ fahr_in_func.c
@@ -7,25 +7,21 @@
 #define UPPER 300 // Верхний предел
 #define STEP 20 // Шаг
 
-int cels2fahr (int upper, int lower);
+int cels2fahr ();
 main()
 {
-  int upper, lower;
-  cels2fahr(upper, lower);
+  cels2fahr(UPPER, LOWER);
   return 0;
 }
 
-int cels2fahr (int upper, int lower)
+int cels2fahr ()
 {
     int fahr;
-    upper = UPPER;
-    lower = LOWER;
-
     printf("\n\n\ncelsius to fahrengeit in function\n\n\n");
-    for(fahr = lower; fahr <= upper; fahr = fahr + STEP)
+    for(fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
     {
        printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32) );
     }  
        printf("\n");
-       return; 
+       return 0; 
 }
